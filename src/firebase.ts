e3dbca16921605a096282aa581e5041b5eedd336
unknown <kurer456@naver.com>
2024-10-10 23:46:38 +0900

    base setting + firebase code added

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD2_yI91_gZhxHlTSFu4yB_6dLI6OlrEEs",
+    authDomain: "nwitter-reloaded-a5635.firebaseapp.com",
+    projectId: "nwitter-reloaded-a5635",
+    storageBucket: "nwitter-reloaded-a5635.appspot.com",
+    messagingSenderId: "404577018672",
+    appId: "1:404577018672:web:50febefbf268c5b52b1ef5",
+};
+
+const app = initializeApp(firebaseConfig);
