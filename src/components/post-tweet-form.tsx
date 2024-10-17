383bec7e4b8e14ac36ee5adfee8f4e9a6d7093b5
unknown <kurer456@naver.com>
2024-10-17 23:14:43 +0900

    delete tweet

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -90,7 +90,8 @@ export default function PostTweetForm() {
             if (file) {
                 const locationRef = ref(
                     storage,
-                    `tweets/${user.uid}-${user.displayName}/${doc.id}`
+                    `tweets/${user.uid}/${doc.id}`
+                    // `tweets/${user.uid}-${user.displayName}/${doc.id}`
                 );
                 const result = await uploadBytes(locationRef, file);
                 const url = await getDownloadURL(result.ref);
--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
     display: grid;
@@ -23,13 +26,44 @@ const Photo = styled.img`
     height: 100px;
     border-radius: 15px;
 `;
-
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+    background-color: tomato;
+    color: white;
+    font-weight: 600;
+    border: 0;
+    font-size: 12px;
+    padding: 5px 10px;
+    text-transform: uppercase;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+    const user = auth.currentUser;
+    const onDelete = async () => {
+        const ok = confirm("Are you sure you want to delete this tweet?");
+        if (!ok || user?.uid !== userId) return;
+        try {
+            //트윗 삭제
+            await deleteDoc(doc(db, "tweets", id));
+            //트윗에 연결된 이미지 삭제
+            if (photo) {
+                const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+                await deleteObject(photoRef);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            //
+        }
+    };
     return (
         <Wrapper>
             <Column>
                 <Username>{username}</Username>
                 <Payload>{tweet}</Payload>
+                {user?.uid === userId ? (
+                    <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+                ) : null}
             </Column>
             <Column>{photo ? <Photo src={photo} /> : null}</Column>
         </Wrapper>
